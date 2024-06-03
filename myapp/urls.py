d548d15f8b17c3627052ce6ab70dd85a283edfe9
PRASANTH <prasanth@gmail.com>
2024-06-03 17:34:42 +0530

    Added delete function

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,15 +4,19 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 
-
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('agents/', AdminandAgentListView.as_view(), name='list-users'),
-    path('agent/<int:pk>/', AdminandAgentUpdateView.as_view(), name='get_agent'),
-    path('create-room/', CreateRoomView.as_view(), name='create-room'),
-    path('rooms/', ListRoomsView.as_view(), name='list-rooms'),
-    path('rooms/<uuid:room_id>/', RoomDetailView.as_view(), name='room-detail'),
-    path('message/<uuid:room_id>/', CreateMessageView.as_view(), name='create_message'),
-    path('messages/<uuid:room_id>/', MessageListView.as_view(), name='get_messages_by_room'),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("agents/", AdminandAgentListView.as_view(), name="list-users"),
+    path("agent/<int:pk>/", AdminandAgentUpdateView.as_view(), name="get_agent"),
+    path("create-room/", CreateRoomView.as_view(), name="create-room"),
+    path("rooms/", ListRoomsView.as_view(), name="list-rooms"),
+    path("rooms/<uuid:room_id>/", RoomDetailView.as_view(), name="room-detail"),
+    path("rooms/<uuid:room_id>/delete/", DeleteRoomView.as_view(), name="delete_room"),
+    path("message/<uuid:room_id>/", CreateMessageView.as_view(), name="create_message"),
+    path(
+        "messages/<uuid:room_id>/",
+        MessageListView.as_view(),
+        name="get_messages_by_room",
+    ),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -10,13 +10,14 @@ from django.views.generic import View
 from .models import AdminandAgent, ChatRoom, Message
 from .serializers import *
 
-#add agent and admin
+
+# add agent and admin
 class RegisterView(generics.CreateAPIView):
     queryset = AdminandAgent.objects.all()
     serializer_class = AdminandAgentSerializer
 
 
-#login agent and admin
+# login agent and admin
 
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
@@ -25,36 +26,42 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from .serializers import AdminandAgentSerializer
 
+
 class LoginView(generics.GenericAPIView):
     serializer_class = AdminandAgentSerializer
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        email = request.data.get('email')
-        password = request.data.get('password')
+        email = request.data.get("email")
+        password = request.data.get("password")
         user = authenticate(email=email, password=password)
 
         if user:
             refresh = RefreshToken.for_user(user)
             role = user.role
             first_name = user.first_name
-            return Response({
-                'refresh': str(refresh),
-                'role': role,
-                'first_name': first_name,
-                'access': str(refresh.access_token),
-            })
-        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {
+                    "refresh": str(refresh),
+                    "role": role,
+                    "first_name": first_name,
+                    "access": str(refresh.access_token),
+                }
+            )
+        return Response(
+            {"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED
+        )
 
 
-#get all agents and admins
+# get all agents and admins
+
 
 class AdminandAgentListView(generics.ListAPIView):
     queryset = AdminandAgent.objects.all()
     serializer_class = AdminandAgentSerializer
 
 
-#edit agent and admin by id
+# edit agent and admin by id
 class AdminandAgentUpdateView(generics.RetrieveUpdateAPIView):
     queryset = AdminandAgent.objects.all()
     serializer_class = AgentEditSerializer
@@ -64,9 +71,11 @@ class AdminandAgentUpdateView(generics.RetrieveUpdateAPIView):
 # chat room details view
 class CreateRoomView(APIView):
     def post(self, request):
-        user_name = request.data.get('user_name') 
+        user_name = request.data.get("user_name")
         if not user_name:
-            return Response({'error': 'User name is required'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "User name is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
         chat_room = ChatRoom(user_name=user_name)
         chat_room.save()  # The page_url is set during save method of the model
@@ -80,29 +89,51 @@ class ListRoomsView(APIView):
         serializer = ChatRoomSerializer(rooms, many=True)
         return Response(serializer.data)
 
+
 class RoomDetailView(APIView):
     def get(self, request, room_id):
         try:
             room = ChatRoom.objects.get(room_id=room_id)
         except ChatRoom.DoesNotExist:
-            return Response({'error': 'Room not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+            return Response(
+                {"error": "Room not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
         serializer = ChatRoomSerializer(room)
         return Response(serializer.data)
-    
+
+
+class DeleteRoomView(generics.DestroyAPIView):
+    queryset = ChatRoom.objects.all()
+    lookup_field = "room_id"
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+        except ChatRoom.DoesNotExist:
+            return Response(
+                {"error": "Room not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        self.perform_destroy(instance)
+        return Response(
+            {"success": "Room deleted successfully"}, status=status.HTTP_200_OK
+        )
 
 
 class StoreMessageView(View):
     def post(self, request):
-        message_data = request.POST.get('message')
-        sender = request.POST.get('sender')
-        room_id = request.POST.get('room_id')
+        message_data = request.POST.get("message")
+        sender = request.POST.get("sender")
+        room_id = request.POST.get("room_id")
 
         # Store the message in the database
-        message = Message.objects.create(room_id=room_id, message=message_data, sender=sender)
+        message = Message.objects.create(
+            room_id=room_id, message=message_data, sender=sender
+        )
 
-        return JsonResponse({'success': True, 'message_id': message.id})
-    
+        return JsonResponse({"success": True, "message_id": message.id})
+
 
 class CreateMessageView(generics.CreateAPIView):
     queryset = Message.objects.all()
@@ -113,19 +144,21 @@ class CreateMessageView(generics.CreateAPIView):
     serializer_class = MessageSerializer
 
     def create(self, request, *args, **kwargs):
-        room_id = self.kwargs.get('room_id')
+        room_id = self.kwargs.get("room_id")
         data = request.data.copy()
-        data['room_id'] = room_id
+        data["room_id"] = room_id
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
 
 class MessageListView(generics.ListAPIView):
     serializer_class = MessageSerializer
 
     def get_queryset(self):
-        room_id = self.kwargs['room_id']
+        room_id = self.kwargs["room_id"]
         return Message.objects.filter(room_id=room_id)
